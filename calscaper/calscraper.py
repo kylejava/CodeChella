a154bd7f7d26a11ae1c375447b44dcc781cc19a7
Kyle <kyleedwardgjava@gmail.com>
2020-11-20 00:41:50 -0800

    started scraping function

--- calscaper/calscraper.py
+++ calscaper/calscraper.py
@@ -11,5 +11,15 @@ def get_images_by_page(page_num):
 	divs = soup.find_all(class_='big_image hide')
 
 	image_urls = [url.find('img')['src'] for url in divs]
-	
+
 	return image_urls
+
+def get_names_for_images(page_num):
+	target = 'https://calscape.org/photos/{0}'.format(page_num)
+	payload = requests.get(target)
+	soup = BeautifulSoup(payload.text, 'lxml')
+	header = soup.find(id="plant_photos")
+	print(header)
+
+
+get_names_for_images(1)
