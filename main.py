a74c344d6dd4c654cf66d6f66afb306316fc138b
Kyle <kyleedwardgjava@gmail.com>
2020-11-19 22:45:45 -0800

    Created Function to reply to user

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import keys as s
 import tweepy
 import json
 import webbrowser
+import time
 from pprint import pprint
 
 
@@ -11,12 +12,15 @@ auth.set_access_token(s.access_token, s.access_token_secret)
 api = tweepy.API(auth)
 
 
-# Grabs the picture from the last mentioned tweet
-mentions = api.mentions_timeline()
-for mention in mentions:
-    pic = (mention.entities['media'][0]['media_url'])
-    break
+# Grabs the picture from the last mentioned tweet'
+def searchForImages():
+    mentions = api.mentions_timeline()
+    for mention in mentions:
+        if('#roots' in mention.text):
+            print(mention.user.screen_name)
+            api.status_update('@' + mention.user.screen_name+ " test worked")
 
 
-
-webbrowser.open(pic)
+while True:
+    searchForImages()
+    time.sleep(15)
