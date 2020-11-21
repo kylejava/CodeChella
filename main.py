4aa1e1f696db4144c622b93ab36799e55ccc1f0b
Kyle <kyleedwardgjava@gmail.com>
2020-11-20 20:04:11 -0800

    Commit leftover changes

--- main.py
+++ main.py
@@ -4,12 +4,31 @@ import json
 import webbrowser
 import time
 from pprint import pprint
+import os, io
+from google.cloud import vision
+import sys
+import requests
+from test import *
 
+#from google.cloud import storage
+#from google.cloud.vision_v1 import enums
+#from google.cloud.vision_v1 import ImageAnnotatorClient
+#from google.cloud.vision_v1 import types
 
 #Replace Keys with Final Twitter Account
 auth = tweepy.OAuthHandler(s.consumer_key, s.consumer_secret)
 auth.set_access_token(s.access_token, s.access_token_secret)
 api = tweepy.API(auth)
+
+#Transfers the image to the Google Vision api
+def identifyImage(image_from_tweet):
+    image = 'https://www.mountroyalseeds.com/wp-content/uploads/2016/09/Abies-lasiocarpa_2-480x640.jpg'
+    client = vision.ImageAnnotatorClient()
+    response = client.annotate_image({
+  'image': {'source': {'image_uri': image}},
+  'features': [{'type': vision.enums.Feature.Type.FACE_DETECTION}],})
+    print(response)
+
 
 
 # Grabs the picture from the last mentioned tweet'
@@ -19,8 +38,3 @@ def searchForImages():
         if('#roots' in mention.text):
             print(mention.user.screen_name)
             api.status_update('@' + mention.user.screen_name+ " test worked")
-
-
-while True:
-    searchForImages()
-    time.sleep(15)
