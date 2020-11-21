424eb8236bffb23bd1173863530ff9f40ff2c24f
Ace <ace.g.fig@gmail.com>
2020-11-20 21:19:43 -0800

    Fixed google's incorrect code example

--- test/predict.py
+++ test/predict.py
@@ -1,20 +1,21 @@
 import sys
+import os
 
-from google.cloud import automl_v1beta1
-from google.cloud.automl_v1beta1.proto import service_pb2
-
+from google.cloud import automl_v1
 
 # 'content' is base-64-encoded image data.
 def get_prediction(content, project_id, model_id):
-    prediction_client = automl_v1beta1.PredictionServiceClient()
+    prediction_client = automl_v1.PredictionServiceClient()
 
     name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
     payload = {'image': {'image_bytes': content }}
     params = {}
-    request = prediction_client.predict(name, payload, params)
+    request = prediction_client.predict(name=name, payload=payload)
     return request  # waits till request is returned
 
 if __name__ == '__main__':
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'CodeChella-315303cc6526.json'
+
     file_path = sys.argv[1]
     project_id = sys.argv[2]
     model_id = sys.argv[3]
