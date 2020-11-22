3d6ce759d9fe3ea9a6bb23e568b9f41feec9f705
Ace <ace.g.fig@gmail.com>
2020-11-21 19:49:23 -0800

    Added additional info

--- calscaper/calscraper_info.py
+++ calscaper/calscraper_info.py
@@ -2,11 +2,16 @@
 
 import calscraper as cs
 
+# Determines how far we step through the gallery to download images
 value = 1000
+# This parameter determines if the plant gallery is sufficient enough to download.
+# If the gallery has less than acceptable_image_count, the gallery will not be downloaded.
+acceptable_image_count = 50
+
 for i in range(value):
 	payload = cs.get_payload_for_page(i+1)
 	number_of_imgs = len(cs.get_images_by_page(payload=payload))
-	if number_of_imgs < 50:
+	if number_of_imgs < acceptable_image_count:
 		continue
 
 	plant_scientific_name = cs.get_name_for_images(payload=payload)
